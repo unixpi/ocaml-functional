d82aa904260f282bb9239e754f6f4571277a1c60
sach <sacha.saint-leger@mail.mcgill.ca>
2015-11-01 18:05:22 -0500

    'add' seems to be working in the general case

--- hw3/hw3-template/dict/dict.ml
+++ hw3/hw3-template/dict/dict.ml
@@ -58,8 +58,10 @@ struct
 
   let rec add (t : dict) (l : char list) =
     if t = [] then [list_to_trie l]
+    else if l = [] then t 		     
     else
       match (hd t) , (hd l) with
+        | End, _ -> [list_to_trie l]
         | Node(elem,children) , elemi -> if elemi = elem then
                                            [Node(elem, add children (tl l))]
                                          else
